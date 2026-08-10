d5224316717ace174a2fbdc99b836d1ecec48ba2
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-10 10:57:41 +0530

    Add files via upload

---
+++ c++ /functions/qsn6.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int multiply(int a, int b){
+return a*b;
+}
+int main() {
+    int a;
+    int b;
+    cin>>a>>b;
+    int result=multiply(a,b);
+    cout<<result + 10;
+}

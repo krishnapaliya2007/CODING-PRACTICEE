fa4b6a99240e44116cfaa9d6ff16e30e831f901b
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-10 10:56:37 +0530

    Add files via upload

---
+++ c++ /functions/qsn4.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int square(int n) {
+    return n*n;
+}
+int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    int result=square(n);
+    cout<< result;
+}

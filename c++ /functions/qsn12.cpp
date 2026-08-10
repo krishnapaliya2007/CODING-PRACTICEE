49899c37a5866c1e445b00481bc91a42c5c096a4
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-10 11:01:32 +0530

    Add files via upload

---
+++ c++ /functions/qsn12.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumOfNumbers(int n) {
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int result = sumOfNumbers(n);
+
+    cout << result;
+}

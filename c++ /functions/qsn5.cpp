4131b4794493d8ec95726ac081eabde98d7dd41c
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-10 10:57:05 +0530

    Add files via upload

---
+++ c++ /functions/qsn5.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int add(int a,int b) {
+    return a+b ;
+}
+int main() {
+    int a;
+    int b;
+    cin>>a>>b;
+    int result=add(a,b);
+    cout<<result;
+}

56af80d110945a39cc85377a0b4d6b0e045bf60c
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-14 09:31:10 +0530

    Add files via upload

---
+++ python/chapter-5/ps7.py
@@ -0,0 +1,7 @@
+# Write a program to find out whether a given post is talking about “Harry” or not.
+post = input("Write your post: ")
+
+if "Harry" in post:
+    print("The post is talking about Harry")
+else:
+    print("The post is not talking about Harry")

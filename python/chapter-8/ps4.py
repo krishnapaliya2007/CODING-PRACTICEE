709756b9c46e23e13d5724e6c91889f0119ee4a6
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-15 11:44:49 +0530

    Add files via upload

---
+++ python/chapter-8/ps4.py
@@ -0,0 +1,6 @@
+# Write a recursive function to calculate the sum of first n natural numbers.
+def sum(a,b):
+    return a+b
+a=float(input("enter number a:"))
+b=float(input("enter number b:"))
+print(sum(a,b))

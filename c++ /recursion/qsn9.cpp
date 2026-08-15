267db092c91bf23ebd94fa8ec98c551500c37a5c
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-15 20:30:20 +0530

    Create qsn9.cpp

---
+++ c++ /recursion/qsn9.cpp
@@ -0,0 +1,21 @@
+// Count the number of digits  
+#include <iostream>
+using namespace std;
+
+int countDigits(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return 1 + countDigits(n / 10);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << countDigits(n);
+
+    return 0;
+}

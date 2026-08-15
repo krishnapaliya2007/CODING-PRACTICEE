53b85abeb3bdfd6c12ad8e37410df59a8f1461fe
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-15 19:33:18 +0530

    Create qsn6.cpp

---
+++ c++ /recursion/qsn6.cpp
@@ -0,0 +1,23 @@
+// Find `a^b` using recursion  
+#include <iostream>
+using namespace std;
+
+int power(int a, int b)
+{
+    if (b == 0)
+        return 1;
+
+    return a * power(a, b - 1);
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter a and b: ";
+    cin >> a >> b;
+
+    cout << power(a, b);
+
+    return 0;
+}

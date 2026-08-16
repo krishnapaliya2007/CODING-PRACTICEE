dd94813c17ed4cdcfdcbc559470cc3314adc34fd
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-17 00:18:16 +0530

    Create qsn2.cpp

---
+++ c++ /arrays/qsn2.cpp
@@ -0,0 +1,23 @@
+// Find the sum of all elements
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: "<<endl;
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: "<<endl;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+int sum=0;
+ for (int i = 0; i < n; i++) {
+     sum+=arr[i];
+ }
+cout<<"sum:"<< sum;
+}

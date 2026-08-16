559019b5700dfccee677de1955e9e662d3a2c658
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-17 00:57:47 +0530

    Create qsn9.cpp

---
+++ c++ /arrays/qsn9.cpp
@@ -0,0 +1,26 @@
+// Reverse an array
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Reverse array: ";
+
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

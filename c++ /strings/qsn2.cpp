61801eef6403c966249a3c259b7166121426060a
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-19 23:23:08 +0530

    Create qsn2.cpp

---
+++ c++ /strings/qsn2.cpp
@@ -0,0 +1,15 @@
+// Find the Length of a String
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int length = str.length();
+
+    cout << "Length = " << length;
+
+    return 0;
+}

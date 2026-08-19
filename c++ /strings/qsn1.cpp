9b421ff18ad68babcc307897170bfcdc1b719ef1
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-08-19 22:55:14 +0530

    Create qsn1.cpp

---
+++ c++ /strings/qsn1.cpp
@@ -0,0 +1,15 @@
+// take a String as Input and Print It
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    cout << "String is: " << str;
+
+    return 0;
+}
